ca7439a021ec50a925de8d68ea4a187bd6a566f7
Terry Vogelsang <terry.vogelsang@react.social>
2018-10-03 01:32:58 +0200

    ♻️ Remove customhttpresponse and logger

--- logger/logger.go
+++
@@ -1,9 +0,0 @@
-package logger
-
-type LogInfos struct {
-	LogID   string `json:"type"`
-	Service string `json:"service"`
-	Type    string `json:"type"`
-	Code    string `json:"code"`
-	Message string `json:"message"`
-}
